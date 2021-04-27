b4773ff3e6d382ac10bcfc72a8fc32ec125298a3
Daniel Zhang <daniel.zhang@vrviu.com>
2021-04-27 18:33:16 +0800

    fixed bug: unable to save

--- routes/articles.js
+++ routes/articles.js
@@ -9,7 +9,7 @@ router.get('/new', (req, res) => {
 
 router.get('/edit/:id', async (req, res) => {
     const article = await Article.findById(req.params.id);
-    res.render('articles/new', {article: article });
+    res.render('articles/edit', {article: article });
 })
 
 router.get('/:slug', async (req, res) => {
@@ -34,17 +34,19 @@ router.put('/:id', async (req, res, next) => {
 }, saveArticleAndRedirect('edit'))
 
 function saveArticleAndRedirect(path) {
-    return async (res, req) => {
-    let article = req.article;
-    article.title = req.body.title;
-    article.description = req.body.description;
-    article.markdown = req.body.markdown;
-    try {
-        article = await article.save();
-        res.redirect(`/articles/${article.slug}`)
-    } catch (e) {
-        res.render('articles/${path}', { article: article})
+        return async (req, res) => {
+        let article = req.article;
+        console.log(req.body)
+        article.title = req.body.title;
+        article.description = req.body.description;
+        article.markdown = req.body.markdown;
+        try {
+            article = await article.save();
+            res.redirect(`/articles/${article.slug}`)
+        } catch (e) {
+            res.render(`articles/${path}`, { article: article})
+        }
     }
-}}
+}
 
 module.exports = router;
